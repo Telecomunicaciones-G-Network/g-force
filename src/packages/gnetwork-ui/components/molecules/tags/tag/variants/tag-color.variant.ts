39e7ec18fa9a8eb6c8ed53530ecbc923a66659c7
IsSact22 <aisaachung@gmail.com>
2026-02-06 17:29:57 -0400

    feature: gforce 142 change of internal messaging variant

--- src/packages/gnetwork-ui/components/molecules/tags/tag/variants/tag-color.variant.ts
+++ src/packages/gnetwork-ui/components/molecules/tags/tag/variants/tag-color.variant.ts
@@ -3,6 +3,6 @@ export const tagColors = {
   gray: ['bg-neutral-500 text-white'],
   green: ['bg-tag-green-background text-tag-green-foreground'],
   purple: ['bg-tag-purple-background text-tag-purple-foreground'],
-  red: ['bg-red-200 text-red-700'],
+  red: ['bg-red-600 text-white'],
   yellow: ['bg-warning-100 text-warning-300'],
 } as const;
