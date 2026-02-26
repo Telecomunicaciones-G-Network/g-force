6392e98929ca14cbb07651015aab450bd7428e4c
IsSact22 <aisaachung@gmail.com>
2026-02-26 17:08:01 -0400

    feature: gforce ui messages documents, options interactive & preview component video

--- src/ui/chat/components/client/sections/chat-conversation/components/chat-text-message-controller/chat-text-message-controller.tsx
+++ src/ui/chat/components/client/sections/chat-conversation/components/chat-text-message-controller/chat-text-message-controller.tsx
@@ -7,27 +7,35 @@ import { isoToTime } from '@timer/utils/iso-to-time.util';
 
 import { MessageDirections } from '@module-chat/domain/enums/message-directions.enum';
 
-import { ChatTextMessage } from '@ui-chat/components/server/messages/chat-text-message';
+import {
+  ChatTextMessage,
+  ChatTextWithLinks,
+} from '@ui-chat/components/server/messages/chat-text-message';
 import { ChatReplyPreview } from '@ui-chat/components/client/messages/chat-reply-preview';
 
 export const ChatTextMessageController = ({
   message,
-}: Readonly<ChatConversationControllerProps>) => (
-  <ChatTextMessage
-    caption={null}
-    direction={
-      message?.direction === MessageDirections.INCOMING
-        ? BubbleModes.INCOMING
-        : BubbleModes.OUTGOING
-    }
-    isBot={message?.sender?.isBot}
-    status={message?.status.toLowerCase() as BubbleStatus}
-    time={isoToTime(message?.createdAt ?? '')}
-    username={message?.sender?.name ?? ''}
-    forwarded={message?.forwarded}
-    forwardedManyTimes={message?.forwardedManyTimes}
-  >
-    <ChatReplyPreview replyToMessage={message?.replyToMessage} />
-    <span className="whitespace-pre-wrap">{message?.text}</span>
-  </ChatTextMessage>
-);
+}: Readonly<ChatConversationControllerProps>) => {
+  const direction =
+    message?.direction === MessageDirections.INCOMING
+      ? BubbleModes.INCOMING
+      : BubbleModes.OUTGOING;
+
+  return (
+    <ChatTextMessage
+      caption={null}
+      direction={direction}
+      isBot={message?.sender?.isBot}
+      status={message?.status.toLowerCase() as BubbleStatus}
+      time={isoToTime(message?.createdAt ?? '')}
+      username={message?.sender?.name ?? ''}
+      forwarded={message?.forwarded}
+      forwardedManyTimes={message?.forwardedManyTimes}
+    >
+      <ChatReplyPreview replyToMessage={message?.replyToMessage} />
+      <span className="whitespace-pre-wrap">
+        <ChatTextWithLinks text={message?.text ?? ''} direction={direction} />
+      </span>
+    </ChatTextMessage>
+  );
+};
